5da3c7554a1a8e373592a324bd333a6097fad008
lolcoder <chetan.arora02@gmail.com>
2022-04-17 09:52:13 +0530

    Dynamic Service Routing + Hostname Resolution

--- src/main/java/com/example/proxy/ReverseProxyApplication.java
+++ src/main/java/com/example/proxy/ReverseProxyApplication.java
@@ -4,12 +4,14 @@ package com.example.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class ReverseProxyApplication {
 
 	public static void main(String[] args) {
